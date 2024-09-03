cd41859914f284ada4ff39f228c04c344d0d20d8
snehamoybag <snehamoybag2@gmail.com>
2024-09-03 22:17:43 +0530

    feat: add styling for item page info section

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,35 @@
+import "../styles/sr-only.css";
+import "../styles/item.css";
 import PropTypes from "prop-types";
+import ItemPrice from "./ItemPrice";
+import Rating from "./Rating";
 
 const Item = ({ item }) => {
   const { image, title, description, price, category, rating } = item;
 
-  // just some placeholder for now
   return (
-    <main className="item">
-      <img src={image} alt={title} className="item__img" />
-      <h1 className="item__name">{title}</h1>
-      <p className="item__description">{description}</p>
-    </main>
+    <section className="item">
+      <div className="item__info">
+        <div className="item__rating-wrapper">
+          <Rating rating={rating.rate} />
+          <p className="item__rating-count">
+            <span className="sr-only"> from</span>
+            <span className="total-rating"> ({rating.count})</span>
+            <span className="sr-only"> ratings</span>
+          </p>
+        </div>
+        <h1 className="item__name">{title}</h1>
+        <img src={image} alt={title} className="item__img" />
+        <ItemPrice price={price} />
+        <span className="item__line"></span>
+        <p className="item__paragraph">
+          <span>Description:</span> {description}
+        </p>
+        <p className="item__paragraph">
+          <span>Tag:</span> {category}
+        </p>
+      </div>
+    </section>
   );
 };
 
