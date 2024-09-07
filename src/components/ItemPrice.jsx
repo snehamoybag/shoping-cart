ed2f90236cbe518bd7e1a7f0a6c2aa20f91901d8
snehamoybag <snehamoybag2@gmail.com>
2024-09-07 19:51:28 +0530

    feat: make price componenent show only og price

--- src/components/ItemPrice.jsx
+++ src/components/ItemPrice.jsx
@@ -5,15 +5,9 @@ import PropTypes from "prop-types";
 
 const ItemPrice = ({ price }) => {
   return (
-    <div className="item-price">
-      <p className="item-price__discount">
-        <span className="sr-only">Discount Price: </span>${price}
-      </p>
-      <p className="item-price__original">
-        <span className="sr-only">Original Price: </span>
-        <s>${(price + 50).toFixed(2)}</s>
-      </p>
-    </div>
+    <p className="item-price">
+      <span className="sr-only">Price: </span>${price}
+    </p>
   );
 };
 
